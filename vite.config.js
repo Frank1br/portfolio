8c6a963089be595753764b1439ce9539de179eb6
Frank Oliveira <fm0325836@gmail.com>
2025-09-16 15:11:56 -0300

    feat: add PWA support with vite-plugin-pwa and create manifest.json

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: { alias: { '@': path.resolve(__dirname, "./src") } },
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'DevFrank Portfolio',
+        short_name: 'DevFrank',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#111827',
+        theme_color: '#111827',
+        icons: [
+          {
+            src: 'public/projects/favicon.png', // ou use um ícone maior específico para PWA
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'public/projects/favicon.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, "./src")
+    }
+  }
 });
-
