fe2cd43f96df0e050bb1cdec7bf511fc1a9ceb72
Frank Oliveira <fm0325836@gmail.com>
2025-09-16 14:35:03 -0300

    chore: remove unnecessary blank lines and update footer comment in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,6 +10,8 @@ import { Footer } from "../components/footer"
 
 
 
+
+
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -27,8 +29,8 @@ export const Home = () => {
             <ProjectsSection />
             <ContactSection />
         </main>
-        {/* Footer */}
-        <Footer />
+        {/* foo */}
+            <Footer />
     </div>
   )
 }
